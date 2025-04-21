ea2cd5833a2533caf4ca1d123c732c6fa79f345e
NAYEONG OH <nayeong.oh@mataedu.com>
2025-04-21 22:30:12 +0900

    expo 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log('ddd');
+  return (
+    <View style={styles.container}>
+      <Text>안녕</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
